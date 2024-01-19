b5f84e349a11b15d193293a3d694fba6d8f1f1a5
opilane <opilane@HP-CZC2349HV1>
2024-01-19 11:43:15 +0200

    Add project files.

---
+++ BogdanViblyyKorduslaused.py
@@ -0,0 +1,46 @@
+﻿#1
+#for
+#r=0
+#for i in range(15): #(0,15,1)
+#    arv=float(input("Sisesta {0}. arv".format(i+1)))
+#    if arv==int(arv):
+#        r+=1
+#print("Täisarvude arv on "+str(r))
+
+
+
+
+#while True
+#r=0
+#i=0
+#while True:
+#    i+=1
+#    arv=float(input("Sisesta {0}. arv".format(i)))
+#    if arv==int(arv):
+#        r+=1
+#    if i==15: break
+#print("Täisarvude arv on "+str(r))
+#r=0
+#i=0
+#while i<15:
+#    i+=1
+#    arv=float(input("Sisesta {0}. arv".format(i)))
+#    if arv==int(arv):
+#        r+=1
+  
+#print("Täisarvude arv on "+str(r))
+
+
+
+#3
+#for
+
+i = 1
+for i in range(8):
+    arv=float(input("Sisesta {0}. arv".format(i)))
+    if arv>0:
+        i *= arv
+print("positiivsete numbrite saamine.", i)
+
+
+        
